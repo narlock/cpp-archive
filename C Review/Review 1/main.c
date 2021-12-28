c57caaea8633ee1e228a255cfc6e55136da7eca9
Anthony <anthonynarlock@outlook.com>
2021-12-28 07:15:44 -0600

    Review C #1

---
+++ C Review/Review 1/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: antho
+ *
+ * Created on December 28, 2021, 7:12 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * A simple hello world program in C
+ */
+int main() {
+    printf("hello world");
+    return (EXIT_SUCCESS);
+}
+
