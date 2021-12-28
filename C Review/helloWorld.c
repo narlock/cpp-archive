067c3cde62d38dc39e48917568cc139f66cf69e7
Anthony <anthonynarlock@outlook.com>
2021-12-28 07:51:35 -0600

    Update helloWorld.c

--- C Review/helloWorld.c
+++ C Review/helloWorld.c
@@ -10,6 +10,15 @@
 
 /*
  * A simple hello world program in C
+ * 
+ * the int main() { } method is the main method
+ * that is called at the beginning of a C program.
+ *
+ * The instructions of the program will begin 
+ * starting at what is in the main method.
+ *
+ * return EXIT_SUCCESS represents the end of the program.
+ * (With a successful run)
  */
 int main() {
     printf("hello world");
